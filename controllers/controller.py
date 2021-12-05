be11dfc3fc835b434819be76eca4946da69573c2
petermordaunt <peetfire@hotmail.com>
2021-12-05 19:44:04 +0000

    added delete button and functionality

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,21 +10,31 @@ def index():
 @app.route('/shopping-list', methods=['POST'])
 def add_task():
     item_name = request.form["item_name"]
-    price = request.form["price"]
-    quantity = request.form["quantity"]
+    price = float(request.form["price"])
+    quantity = int(request.form["quantity"])
     new_item = Item(item_name, price, quantity)
     add_new_item(new_item)
+    set_totals()
     return redirect('/shopping-list')
 
 @app.route('/shopping-list/bought', methods=['POST'])
 def set_bought():
     item = request.form['bought_button']
     set_item_bought(item, True)
+    set_totals()
     return redirect('/shopping-list')
 
 @app.route('/shopping-list/uncheck', methods=['POST'])
 def set_not_bought():
     item = request.form['bought_button']
     set_item_bought(item, False)
+    set_totals()
+    return redirect('/shopping-list')
+    
+@app.route('/shopping-list/delete', methods=['POST'])
+def delete_item():
+    item = request.form['delete_button']
+    remove_item_by_name(item)
+    set_totals()
     return redirect('/shopping-list')
     
--- models/shopping_list.py
+++ models/shopping_list.py
@@ -13,6 +13,11 @@ headings = ["Item name", "Price", "Quantity", "Bought?"]
 def add_new_item(item):
     shopping_list.append(item)
 
+def remove_item_by_name(name):
+    for item in shopping_list:
+        if item.item_name == name:
+            shopping_list.remove(item)
+
 def set_item_bought(name, status):
     for item in shopping_list:
         if item.item_name == name:
@@ -27,9 +32,17 @@ def get_total_items(item_list):
 def get_total_bought(item_list):
     return sum([item.quantity for item in item_list if item.bought])
 
-set_item_bought("Butter", True)
 total_price = get_total_price(shopping_list)
 total_items = get_total_items(shopping_list)
 total_bought = get_total_bought(shopping_list)
 grand_totals = [total_price, total_items, total_bought]
+
+def set_totals():
+    total_price = get_total_price(shopping_list)
+    total_items = get_total_items(shopping_list)
+    total_bought = get_total_bought(shopping_list)
+    grand_totals.clear()
+    grand_totals.append(total_price)
+    grand_totals.append(total_items)
+    grand_totals.append(total_bought)
 
